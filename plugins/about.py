34529b82d55887ff6f039a1875c51c76be9e1414
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-05-30 23:46:01 +0530

    Update about.py

--- plugins/about.py
+++ plugins/about.py
@@ -1,6 +1,6 @@
 import os 
 from pyrogram import Client, filters
-token = os.environ.get('TOKEN','')
+token = os.environ.get('TOKEN','6295603578:AAEdxq3Nh5zPKMnr-1RfgosDl5Q10Nwj54M')
 botid = token.split(':')[0]
 from helper.database import botdata, find_one, total_user
 from helper.progress import humanbytes
