a4f6731b9d1582246f42850ac72759c349b05505
PyroUserBot <129763781+PyroUserBot@users.noreply.github.com>
2023-05-31 09:08:02 +0530

    Update thumbfunction.py

--- plugins/thumbfunction.py
+++ plugins/thumbfunction.py
@@ -8,17 +8,17 @@ async def viewthumb(client,message):
 		if thumb :
 			await client.send_photo(message.chat.id,photo =f"{thumb}")
 		else:
-			await message.reply_text("**You don't have any custom thumbnail**")
+			await message.reply_text("**ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴀɴʏ ᴄᴜsᴛᴏᴍ ᴛʜᴜᴍʙɴᴀɪʟ**")
 	
 	
 @Client.on_message(filters.private & filters.command(['delthumb']))
 async def removethumb(client,message):
 	delthumb(int(message.chat.id))
-	await message.reply_text("**Custom thumbnail deleted successfully**")
+	await message.reply_text("**ᴄᴜsᴛᴏᴍ ᴛʜᴜᴍʙɴᴀɪʟ ᴅᴇʟᴇᴛᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ 💔**")
 
 @Client.on_message(filters.private & filters.photo)
 async def addthumbs(client,message):
 	file_id = str(message.photo.file_id)
 	addthumb(message.chat.id , file_id)
-	await message.reply_text("**Custom thumbnail saved successfully** ✅")
+	await message.reply_text("**ᴄᴜsᴛᴏᴍ ᴛʜᴜᴍʙɴᴀɪʟ sᴀᴠᴇᴅ sᴜᴄᴄᴇssғᴜʟʟʏ** ✅")
 	
