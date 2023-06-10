8053ea013a7db77f83a07e79aec2c68ee9c03670
Sankar8098 <113809269+Sankar8098@users.noreply.github.com>
2023-06-10 16:46:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
  API_ID    = os.environ.get("API_ID", "23990433")
 
-    API_HASH  = os.environ.get("API_HASH", "e6c4b6ee1933711bc4da9d7d17e1eb20")
+API_HASH  = os.environ.get("API_HASH", "e6c4b6ee1933711bc4da9d7d17e1eb20")
 
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5992501587:AAHdDLpHnleGcpCQUizT1efHLhDNYUrtuYA") 
+    TOKEN = os.environ.get("BOT_TOKEN", "5992501587:AAHdDLpHnleGcpCQUizT1efHLhDNYUrtuYA") 
 
    
 
@@ -12,16 +12,16 @@
 
     DB_URL  = os.environ.get("DB_URL","mongodb+srv://nakflixbot:alpha3720@cluster0.qgybxbu.mongodb.net/?retryWrites=true&w=majority")
 
- 
+ STRING
 
     # other configs
 
-    BOT_UPTIME  = time.time()
 
-    START_PIC   = os.environ.get("START_PIC", "")
 
-    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '').split()]
 
-    FORCE_SUB   = os.environ.get("FORCE_SUB", "5821871362") 
+
+    ADMIN       = [int(admin) if id_pattern.search(admin) else admiadm
+
+
 
     LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", None))
